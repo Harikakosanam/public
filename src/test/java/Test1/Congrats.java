beea95bf392538ad8ca237b14497bc85461937d8
u <u@DESKTOP-PDKOP6A>
2023-01-04 16:11:06 +0530

    HAI

---
+++ src/test/java/Test1/Congrats.java
@@ -0,0 +1,11 @@
+package Test1;
+
+public class Congrats {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("congrats you have selected for test engineer!!!!!");
+	}
+
+}
---
+++ src/test/java/Test1/GoogMorning.java
@@ -0,0 +1,10 @@
+package Test1;
+
+public class GoogMorning {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
